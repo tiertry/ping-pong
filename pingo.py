3356e3ad28aed108ed8ad0919b5deabb08671e9c
tiertry <167616623+tiertry@users.noreply.github.com>
2024-05-11 13:07:12 +0300

    added ball movement

--- pingo.py
+++ pingo.py
@@ -34,21 +34,42 @@ class Player(GameSprite):
         if keys[K_s] and self.rect.y < win_height - 80:
             self.rect.y += self.speed  
 
-racket1 = Player('racket.png', 30,200,4,50,150)  
-racket2 = Player('racket.png', 620,200,4,50,150)   
+racket1 = Player('racket.png', 30,200,13,50,150)  
+racket2 = Player('racket.png', 620,200,13,50,150)   
 ball = GameSprite('pngwing.com.png', 325, 200, 4,50,50) 
 game = True 
 finish = False
+speed_x = 8
+speed_y = 8
+font.init()
+font1 = font.Font(None, 77)
+lost = font1.render('player left lose',True,(180,0,0))
+lost2 = font1.render('player right lose',True,(180,0,0))
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
     if not finish:
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
         main_win.fill(back)
         racket1.updateL()
         racket2.updateR()
         racket1.reset()
         racket2.reset()
         ball.reset()
-        display.update()
-        time.delay(50)
+    if ball.rect.y > win_height - 50 or ball.rect.y < 0:
+        speed_y *= -1   
+    if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+        speed_x *= -1
+    if ball.rect.x <0:
+        finish = True
+        main_win.blit(lost,(200,200))
+        game = True
+    if ball.rect.x > win_width:
+        finish = True
+        main_win.blit(lost2,(200,200))
+        game = True
+    display.update()
+    time.delay(50)
+
